fe42b543c8a86017900cc82da5291931afb05e04
Andres Camilo Jimenez Alvarez <jime112372@siigo.com>
2020-03-21 12:11:00 -0500

    Se agrega un segundo ejemplo

--- Scripts/script1.js
+++ Scripts/script1.js
@@ -3,16 +3,16 @@ var arr;
 function adicionEventos() {
   arr = document.querySelectorAll('li');
 
-  for (var i = 0; i < arr.length; i++) {
-    arr[i].addEventListener('mouseover', function() {
+  for (obj of arr) {
+    obj.addEventListener('mouseover', function() {
       this.classList.add('movimiento');
     });
 
-    arr[i].addEventListener('mouseout', function() {
+    obj.addEventListener('mouseout', function() {
       this.classList.remove('movimiento');
     });
 
-    arr[i].addEventListener('click', function() {
+    obj.addEventListener('click', function() {
       this.classList.add('completado');
     });
   }
